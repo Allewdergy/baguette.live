b1ec9d6bb8809bd0c124fcf057cbe2317a871557
shehatestre <48776628+Allewdergy@users.noreply.github.com>
2021-09-17 16:32:31 +0000

    bug SyntaxError: Unexpected token 'var'

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
+var x = document.getElementById("myTopnav");
+
 function myFunction() {
-    var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
     } else {
       x.className = "topnav";
     }
-  }
+}
